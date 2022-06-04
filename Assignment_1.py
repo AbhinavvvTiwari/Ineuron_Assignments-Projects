4e2ad93ba9b8e86d72c52d7a8ac744b917f55f95
AbhinavvvTiwari <104686236+AbhinavvvTiwari@users.noreply.github.com>
2022-06-04 17:04:55 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# A=1: Expressions: *,-,/,+
+#      Values: 'hello',-87.8,6
+
+# A=2: String is a data type.
+#      Variable stores the data type such as strings. a memory gets allocated in the ram which defined by the variable when a      variable is called it invokes the data stored in the variable by going to the memory
+
+# A=3: Int: it indicates the data type is an integer e.g. 2 , 5 , etc
+#      float: it indiactes the data type has decimal values
+#      str: it indicates the data type has alphabets e.g "Abhi" enclosed by single or double inverted commas
+
+# A=4: Expression is basically used to perform various operations such as concatation, subtraction, additon, on similar data types
+#     it consists of operators like +,-,/*
+
+# A=5: spam = 10 is a statement as it indicates that in python a variable is created called spam which has an integer data type value 10 . so whenever spam will be called in the programme due to the statement spam = 10 we will get the output of 10. it executes the statement
+# whereas expression can perform operations and can even change the value of spam using operators like +,- for e.g. below code
+# 
+
+# In[1]:
+
+
+spam=10
+spam
+
+
+# In[3]:
+
+
+spam=10+10
+spam
+
+
+# as we can see in above code statement does not change the value it is only used to execute the program whereas the expression + changed the value
+# 
+
+# A=6: 23
+
+# A=7:spamspamspam
+#     spamspamspam
+# 
+
+# In[7]:
+
+
+'spam'*3
+
+
+# A=8: 100 is not a valid variable name because 100 is an integer value which corresponds to int data type when you declare the 100 a variable system will not create a space in memory to store values as it will interpret is as data type only
+# however eggs is a valid variable since it begins with an alphabet and is not a standard pre defined data type in programming languae
+# 
+
+# A=9: int() , float() , and str( )
+# 
+# for e.g let's try to get the int,float and str value of 235.343435
+# 
+
+# In[8]:
+
+
+int(235.343435)
+
+
+# In[9]:
+
+
+float(235.343435)
+
+
+# In[10]:
+
+
+str(235.343435)
+
+
+# A=10: I have eaten &#39; + 99 + &#39; burritos.&#39;
+# 
+# concotation is only done for homogenous data type. we can fix the error by enclosing the integer 99 in single/double inverted commas
